aa0041094b36c27430464cf59cf553540548cfc9
analog-sombra <analogsombra@gmail.com>
2024-07-10 16:53:17 +0530

    code updated

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
-use std::process::Command;
-
-use cron_job::CronJob;
+use std::{process::Command, time::Duration};
+use job_scheduler::{Job, JobScheduler};
 
 #[tokio::main]
 async fn main() {
     // Create CronJob
-    let mut cron = CronJob::default();
-    // Add the function
-    // cron.new_job("* * * * * *", run_on_cron);
-    cron.new_job("45 16 * * *", run_on_cron);
-    // Start job
-    let _ = cron.start();
-}
+    // let mut cron = CronJob::default();
+    // // Add the function
+    // cron.new_job("* * * * * *", run_each_second);
+    // cron.new_job("45 16 * * *", run_on_cron);
+    // // Start job
+    // let _ = cron.start();
 
-fn run_on_cron() {
-    let mut command = Command::new("bash");
-    command.arg("/root/script/full_bkp.sh");
+    // loop {
+    //     std::thread::sleep(Duration::from_millis(500));
+    // }
+
+    let mut sched = JobScheduler::new();
+
+    sched.add(Job::new("* 55 16 * * *".parse().unwrap(), || {
+        let mut command = Command::new("bash");
+        command.arg("/root/script/full_bkp.sh");
+    }));
+
+    sched.add(Job::new("* * * * * *".parse().unwrap(), || {
+        println!("i run every second");
+    }));
+
+    loop {
+        sched.tick();
+
+        std::thread::sleep(Duration::from_millis(500));
+    }
 }
